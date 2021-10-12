871bf3e59861f2db7a9a42c6f45ea36c41cb878a
James McKenzie <j.mckenzie@adinstruments.com>
2021-10-13 11:49:04 +1300

    Added solution to finding exact answers of cube roots (Note, not at all safe!)

--- C1_Building_Abstractions_With_Procedures/Ex1.8.scm
+++ C1_Building_Abstractions_With_Procedures/Ex1.8.scm
@@ -9,7 +9,10 @@
 
 
 (define (good-enough? guess x)
-(< (abs (- (improve-cubic guess x) guess)) (* guess 0.001)))
+(< (abs (- (improve-cubic guess x) guess)) (* guess 0.00001)))
+
+(define (exact? guess x)
+(= (* (* guess guess) guess) x))
 
 (define (cubic-iter guess x) 
         (if (good-enough? guess x)
@@ -17,8 +20,16 @@
         (cubic-iter (improve-cubic guess x) x)
         ))
 
+(define (cubic-iter-exact guess x) 
+        (if (exact? guess x)
+        guess
+        (cubic-iter-exact (improve-cubic guess x) x)
+        ))
+
+
 (define (improve-cubic guess x)
        (/ (+ (/ x (* guess guess)   ) (* 2 guess)) 3))
 
 
-(cubic-iter 1.1 5)
+(cubic-iter 1.1 64)
+(cubic-iter-exact 1.1 64)
