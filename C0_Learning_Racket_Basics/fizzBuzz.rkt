9fb284ae5e3a8aa5e38ddd07b53e86f8a83d80b0
James McKenzie <j.mckenzie@adinstruments.com>
2021-10-14 15:44:19 +1300

    Broken Recursion. Please @tA advice what to do lmao

--- C0_Learning_Racket_Basics/fizzBuzz.rkt
+++ C0_Learning_Racket_Basics/fizzBuzz.rkt
@@ -9,7 +9,29 @@
         
         
 (define (fizzBuzz f b ls)
+
     (map ( lambda (x) (fizzBuzzer x 3 5))  ls )
 )
 
+
+
+(define (recursive-fizz-buzz x n f b)
+    (if (< x n)
+    (
+    (cond 
+        ((and (= (modulo x f) 0) (= (modulo x b) 0) ) (display "fizzbuzz\n" ) )
+        ((= (modulo x f) 0) (display "fizz\n")  )
+        ((=  (modulo x b) 0) (display "buzz\n") )
+        (else (display x)  )
+    )
+
+    (recursive-fizz-buzz (+ x 1) n f b )
+    )
+    )
+)
+
+
+
 (fizzBuzz 3 5 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32))
+
+;;;(recursive-fizz-buzz 1 6 3 5)
