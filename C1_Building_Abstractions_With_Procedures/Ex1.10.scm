bffbe5e50b3a26a45f83cf6b123e5119d6f24449
James McKenzie <j.mckenzie@adinstruments.com>
2021-10-22 16:10:26 +1300

    Added a more common Ackerman function to see how long it takes to run

--- C1_Building_Abstractions_With_Procedures/Ex1.10.scm
+++ C1_Building_Abstractions_With_Procedures/Ex1.10.scm
@@ -6,6 +6,13 @@
 ((= y 1) 2)
 (else (A (- x 1) (A x (- y 1))))))
 
+(define (A2 x y)
+(cond ((= x 0) (+ y 1))
+       ((= y 0) (A2 (- x 1) 1))
+       (else (A2 (- x 1) (A2 x (- y 1))))
+       
+       ))
+(A2 4 2)
 ;;;f(n) = 2n
 ;;;g(n) = 2^n
 ;;;h(n) = 2^n^n(nTimes) for n>1
